87e53eca821be264172f72f975aa77d3f66c5b6e
Victor Huerlimann <victor.huerlimann@sisa.ch>
2021-09-27 12:31:54 +0200

    added performance mode

--- main.c
+++ main.c
@@ -11,8 +11,64 @@
 #include "flir_lepton35.h"
 #include <proc/p32mz1024ech064.h>
 
+void set_performance_mode()
+{   
+	unsigned int cp0;
+	
+    // Unlock Sequence
+    asm volatile("di"); // Disable all interrupts
+    SYSKEY = 0xAA996655;
+    SYSKEY = 0x556699AA;  
+
+    // PB1DIV
+    // Peripheral Bus 1 cannot be turned off, so there's no need to turn it on
+    PB1DIVbits.PBDIV = 1; // Peripheral Bus 1 Clock Divisor Control (PBCLK1 is SYSCLK divided by 2)
+
+    // PB2DIV
+    PB2DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
+    PB2DIVbits.PBDIV = 1; // Peripheral Bus 2 Clock Divisor Control (PBCLK2 is SYSCLK divided by 2)
+
+    // PB3DIV
+    PB3DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
+    PB3DIVbits.PBDIV = 1; // Peripheral Bus 3 Clock Divisor Control (PBCLK3 is SYSCLK divided by 2)
+
+    // PB4DIV
+    PB4DIVbits.ON = 1; // Peripheral Bus 4 Output Clock Enable (Output clock is enabled)
+    while (!PB4DIVbits.PBDIVRDY); // Wait until it is ready to write to
+    PB4DIVbits.PBDIV = 0; // Peripheral Bus 4 Clock Divisor Control (PBCLK4 is SYSCLK divided by 1)
+
+    // PB5DIV
+    PB5DIVbits.ON = 1; // Peripheral Bus 5 Output Clock Enable (Output clock is enabled)
+    PB5DIVbits.PBDIV = 1; // Peripheral Bus 5 Clock Divisor Control (PBCLK5 is SYSCLK divided by 2)
+
+    // PB7DIV
+    PB7DIVbits.ON = 1; // Peripheral Bus 7 Output Clock Enable (Output clock is enabled)
+    PB7DIVbits.PBDIV = 0; // Peripheral Bus 7 Clock Divisor Control (PBCLK7 is SYSCLK divided by 1)
+
+    // PB8DIV
+    PB8DIVbits.ON = 1; // Peripheral Bus 8 Output Clock Enable (Output clock is enabled)
+    PB8DIVbits.PBDIV = 1; // Peripheral Bus 8 Clock Divisor Control (PBCLK8 is SYSCLK divided by 2)
+
+    // PRECON - Set up prefetch
+    PRECONbits.PFMSECEN = 0; // Flash SEC Interrupt Enable (Do not generate an interrupt when the PFMSEC bit is set)
+    PRECONbits.PREFEN = 0b11; // Predictive Prefetch Enable (Enable predictive prefetch for any address)
+    PRECONbits.PFMWS = 0b010; // PFM Access Time Defined in Terms of SYSCLK Wait States (Two wait states)
+
+    // Set up caching
+    cp0 = _mfc0(16, 0);
+    cp0 &= ~0x07;
+    cp0 |= 0b011; // K0 = Cacheable, non-coherent, write-back, write allocate
+    _mtc0(16, 0, cp0);  
+
+    // Lock Sequence
+    SYSKEY = 0x33333333;
+    asm volatile("ei"); // Enable all interrupts
+}
+
 int main(void)
 {
+    set_performance_mode();
+    
     // Initialize the NOCTIX-1 module
     BSP_Initialize();
     
